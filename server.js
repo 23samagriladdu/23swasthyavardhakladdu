49e9e552c5479fb375132e30b4aaa90283da3640
23samagriladdu <ramesh7con@gmail.com>
2026-08-16 16:40:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
+  res.sendFile(path.join(__dirname, "admin.html"));
 });
 
 app.listen(PORT, () => {
