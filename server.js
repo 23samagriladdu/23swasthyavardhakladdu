13a9d6b8bc44bb81120a9d2d7300e834905660cc
23samagriladdu <ramesh7con@gmail.com>
2026-08-16 15:15:39 +0530

    Serve static files and add root route

--- server.js
+++ server.js
@@ -143,7 +143,11 @@ app.patch("/api/admin/orders/:id", requireAdmin, (req, res) => {
 
   res.json({ success: true });
 });
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
