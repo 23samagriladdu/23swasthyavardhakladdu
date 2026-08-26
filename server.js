81e8ebbc7c2c8631c7651bdb4a8a032823a099ff
23samagriladdu <ramesh7con@gmail.com>
2026-08-26 09:56:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -230,11 +230,7 @@ app.use(
    STATIC WEBSITE
 ========================================================= */
 
-app.use(
-  express.static(
-    path.join(__dirname, "public")
-  )
-);
+app.use(express.static(__dirname));
 
 
 /* =========================================================
